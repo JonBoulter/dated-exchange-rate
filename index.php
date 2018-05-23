429b727fb85c95c00fd1206d7d6ffeb8fe433b86
Blujay75 <jonsmail75@gmail.com>
2018-05-23 12:45:53 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php
 echo "Running PHP";
+
+foreach($_POST as $key => $value)
+{
+    if (strstr($key, 'item'))
+    {
+        $x = str_replace('item','',$key);
+        echo $value . ' : ' . $x;
+    }
+}
+
 ?>
